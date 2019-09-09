1ae92e5a19d997cd432871db91cd9d4b30cde70b
Neto Chaves <netobac1@gmail.com>
2019-09-09 14:54:38 -0300

    create basic components

---
+++ src/components/board.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Knight from './knight'
+import Square from './square'
+
+const Board = () => (
+  <Square black>
+    <Knight />
+  </Square>
+)
+
+export default Board
